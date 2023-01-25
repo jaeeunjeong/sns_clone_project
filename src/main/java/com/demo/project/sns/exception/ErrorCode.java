38cba9b2793ceb29299197d6295a9568ee466972
jaeeunjeong <jejeong000@gmail.com>
2023-01-25 23:22:09 +0900

    add : 로그인 및 회원 가입 로직 추가

--- src/main/java/com/demo/project/sns/exception/ErrorCode.java
+++ src/main/java/com/demo/project/sns/exception/ErrorCode.java
@@ -7,7 +7,13 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "user name is duplicated"),
+    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated"),
+
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid"),
+
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not founded")
+
     ;
 
     private HttpStatus status;
--- src/main/java/com/demo/project/sns/service/UserService.java
+++ src/main/java/com/demo/project/sns/service/UserService.java
@@ -5,25 +5,34 @@ import com.demo.project.sns.exception.SnsApplicationException;
 import com.demo.project.sns.model.User;
 import com.demo.project.sns.model.entity.UserEntity;
 import com.demo.project.sns.repository.UserEntityRepository;
+import com.demo.project.sns.util.JwtTokenUtils;
 import lombok.RequiredArgsConstructor;
-import net.bytebuddy.implementation.bytecode.Throw;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserEntityRepository userEntityRepository;
+    private final BCryptPasswordEncoder encoder;
 
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${jwt.token.expired-time-ms}")
+    private Long expiredTimeMs;
+
+    @Transactional
     public User join(String userName, String password) {
 
         userEntityRepository.findByUserName(userName).ifPresent(it -> {
             throw new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME, String.format("%s is Duplicated", userName));
         });
 
-        UserEntity userEntity = userEntityRepository.save(UserEntity.of(userName, password));
+        UserEntity userEntity = userEntityRepository.save(UserEntity.of(userName, encoder.encode(password)));
 
         return User.fromEntity(userEntity);
     }
@@ -31,10 +40,14 @@ public class UserService {
     public String login(String userName, String password) {
 
         UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() ->
-                new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME, String.format("%s is Duplicated", userName)));
+                new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
 
-        if (!userEntity.getPassword().equals(password))
+
+        if (encoder.matches(password, userEntity.getPassword()))
+            //if (!userEntity.getPassword().equals(password))
             throw new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME, String.format("%s is Duplicated", userName));
+
+        JwtTokenUtils.generateToken(userName, secretKey, expiredTimeMs);
 
         return "";
     }
